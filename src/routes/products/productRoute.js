9806fff5611cf1300330c20e29055cecec41d3e5
maqombc <maqom.bc@gmail.com>
2024-05-13 21:51:42 +0000

    fixing product validators

--- src/routes/products/productRoute.js
+++ src/routes/products/productRoute.js
@@ -2,21 +2,12 @@ import router from 'express';
 import Validate from '../../validators/index.js';
 import { authentication } from '../../middlewares/authentication.js';
 import {
-  organizationNewContributionProduction,
-  organizationNewOrganizationLoanProductSchema,
-  organizationNewOrganizationSavingsProductSchema,
-  organizationsubscriptionProductSchema,
+  organizationNewProduction,
   viewSingleProductSchema
 } from '../../validators/productsSchema.js';
 import {
-  createNewContributionProductHandler,
-  createNewLoanProductHandler,
-  createNewSavingsProductHandler,
-  createSubscriptionHandler,
-  fetchContributionProductHandler,
-  fetchLoanProductHandler,
-  fetchSavingsProductHandler,
-  fetchSubscriptionProductHandler,
+  createNewProductHandler,
+  fetchProductHandler,
   getAllProductsHandler,
   getSingleProductHandler,
   updateSingleProductHandler
@@ -26,60 +17,27 @@ const productRoutes = router.Router();
 
 const productRoot = () => {
   productRoutes.post(
-    '/create-contribution-products',
-    Validate(organizationNewContributionProduction),
+    '/create-products',
+    Validate(organizationNewProduction),
     authentication,
-    createNewContributionProductHandler
+    createNewProductHandler
   );
   productRoutes.get(
-    '/fetch-contribution-products',
+    '/fetch-products',
     authentication,
-    fetchContributionProductHandler
-  );
-
-  // ---------Loan Section of Products------------\\
-
-  productRoutes.post(
-    '/create-loan-products',
-    Validate(organizationNewOrganizationLoanProductSchema),
-    authentication,
-    createNewLoanProductHandler
-  );
-
-  productRoutes.get('/fetch-loan-products', authentication, fetchLoanProductHandler);
-
-  // ----------------savings Sections of Products------------\\
-  productRoutes.post(
-    '/create-savings-products',
-    Validate(organizationNewOrganizationSavingsProductSchema),
-    authentication,
-    createNewSavingsProductHandler
-  );
-  productRoutes.get('/fetch-savings-products', authentication, fetchSavingsProductHandler);
-
-  // -----------------subscription Sectioin of Poducts------------------------\\
-  productRoutes.post(
-    '/create-subscription-product',
-    Validate(organizationsubscriptionProductSchema),
-    authentication,
-    createSubscriptionHandler
-  );
-  productRoutes.get(
-    '/fetch-subscription-products',
-    authentication,
-    fetchSubscriptionProductHandler
+    fetchProductHandler
   );
 
   //------common Section of Products------------\\
   productRoutes.get('/get-all-products', authentication, getAllProductsHandler);
   productRoutes.get(
-    '/view-single-product/:product_id/:product_type',
+    '/view-single-product/:product_id',
     Validate(viewSingleProductSchema, 'params'),
     authentication,
     getSingleProductHandler
   );
   productRoutes.patch(
-    '/update-single-product/:product_id/:product_type',
+    '/update-single-product/:product_id',
     Validate(viewSingleProductSchema, 'params'),
     authentication,
     updateSingleProductHandler
