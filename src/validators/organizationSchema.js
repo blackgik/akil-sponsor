e0d68d3540fc4f06e35c5b14059690c1951d030f
maqombc <maqom.bc@gmail.com>
2024-05-18 10:02:29 +0000

    fixing onboarding schema validator to exclude admin name

--- src/validators/organizationSchema.js
+++ src/validators/organizationSchema.js
@@ -1,58 +1,26 @@
 import Joi from 'joi';
 
 export const validateOnboardingOrganizationSchema = Joi.object({
-  language: Joi.string().optional().allow('', null),
-  name_of_cooperation: Joi.string().optional().allow('', null),
   firstname: Joi.string().optional().allow('', null),
   lastname: Joi.string().optional().allow('', null),
   gender: Joi.string().optional().allow('', null),
   email: Joi.string().email().required(),
   address: Joi.string().optional().allow('', null),
-  admin_name: Joi.string().optional().allow('', null),
   country: Joi.string().optional().allow('', null),
   phone: Joi.string().optional().required(),
-  website: Joi.string().optional().allow('', null),
-  modules: Joi.array().items(Joi.string().optional().allow('', null)),
-  total_number_of_beneficiaries_chosen: Joi.number().optional().default(0),
   reg_fee: Joi.number().optional().allow(0, null),
   slug: Joi.string().required(),
-  bank_details: Joi.object({
-    bvn: Joi.string().optional().allow('', null),
-    bank_name: Joi.string().optional().allow('', null),
-    acct_no: Joi.string().optional().allow('', null),
-    acct_name: Joi.string().optional().allow('', null),
-    bank_code: Joi.string().optional().allow('', null)
-  }).optional(),
-  credit_balance: Joi.number().optional().allow(0, null),
-  deduction_uploads: Joi.number().optional().allow(0, null),
-  agent_referral_code: Joi.string().optional().allow('', null),
   tosAgreement: Joi.boolean().required(),
   pdsAgreement: Joi.boolean().required(),
-  organization_reg_fee: Joi.number().optional().allow(0, null),
-  payment_plan: Joi.string()
-    .optional()
-    .valid('basic', 'standard', 'premium', 'ultimate', 'third_party_api').default('basic'),
-  annual_plan: Joi.boolean().optional().default(false)
 });
 
 export const updateOrganizationProfileSchema = Joi.object({
   language: Joi.string().optional().allow('', null),
   avatar: Joi.object({ key: Joi.string().optional().allow('', null) }).optional(),
-  name_of_cooperation: Joi.string().optional().allow('', null),
   address: Joi.string().optional().allow('', null),
-  admin_name: Joi.string().optional().allow('', null),
   phone: Joi.string().optional().allow('', null),
-  website: Joi.string().optional().allow('', null),
   slug: Joi.string().optional().allow('', null),
-  organization_reg_fee: Joi.number().optional().allow(0, null),
-  reg_fee: Joi.number().optional(),
-  bank_details: Joi.object({
-    bvn: Joi.string().optional().allow('', null),
-    bank_name: Joi.string().optional().allow('', null),
-    acct_no: Joi.string().optional().allow('', null),
-    acct_name: Joi.string().optional().allow('', null),
-    bank_code: Joi.string().optional().allow('', null)
-  }).optional(),
+  reg_fee: Joi.number().optional()
 });
 
 export const validateLoginOrganizationSchema = Joi.object({
