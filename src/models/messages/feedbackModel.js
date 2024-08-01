7fabec2e35ad28ec1626bf57d9ed4fb995af7b7c
Nathan-Okolo <okolonathanchinedu1@gmail.com>
2024-08-01 15:22:51 +0100

    k

--- src/models/messages/feedbackModel.js
+++ src/models/messages/feedbackModel.js
@@ -13,7 +13,7 @@ const feedbackSchema = new Schema(
       trim: true
     },
     request_id: { type: Schema.Types.ObjectId, ref: 'Sponsor_Request' },
-    project_id: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
+    project_id: { type: Schema.Types.ObjectId, ref: 'Project' },
     is_read: { type: Boolean, default: false },
     beneficiary_id: { type: Schema.Types.ObjectId, required: true, ref: 'Organization_Member' },
     sponsor_id: { type: Schema.Types.ObjectId, required: true, ref: 'Organization' }
