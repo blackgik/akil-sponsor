9db156f27fbd20c33d290cb065f4710c224ab88a
blackgik <echendu.chinedum@gmail.com>
2024-08-08 10:36:27 +0100

    Fetch beneficiaries

--- src/routes/donor/donor.route.js
+++ src/routes/donor/donor.route.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import validators from '../../validators/index.js';
 import { createDonorSchema } from '../../validators/donor.schema.js';
-import { createDonorHandler } from '../../controllers/donor/donor.controller.js';
+import {
+  createDonorHandler,
+  dashboardStatisticsHandler,
+  fetchBeneficiariesHandler
+} from '../../controllers/donor/donor.controller.js';
+import { authentication } from '../../middlewares/authentication.js';
 
 const donorROute = Router();
 
 donorROute.post('/create-donor', validators(createDonorSchema), createDonorHandler);
+donorROute.get('/statistics', authentication, dashboardStatisticsHandler);
+donorROute.get('/fetch-beneficiaries', authentication, fetchBeneficiariesHandler);
 
 export default donorROute;
--- src/services/donor/donor.service.js
+++ src/services/donor/donor.service.js
@@ -1,4 +1,5 @@
 import { DuplicateError, InternalServerError, NotFoundError } from '../../../lib/appErrors.js';
+import sponsorshipRequestModel from '../../models/beneficiaries/sponsorshipRequestModel.js';
 import donor from '../../models/donor/donor.js';
 import organizationModel from '../../models/organizationModel.js';
 import rolepermissionModel from '../../models/settings/rolepermission.model.js';
@@ -55,4 +56,131 @@ export const createDonor = async ({ body }) => {
   const userAcount = await craeteNewUser({ user, body: bodyData });
 
   return userAcount;
+};
+
+export const dashboardStatistics = async ({ user }) => {
+  try {
+    const resultCompleted = await sponsorshipRequestModel.aggregate([
+      {
+        $match: { request_state: 'completed' }
+      },
+      {
+        $group: {
+          _id: null, // Grouping by null groups all documents
+          totalAmount: { $sum: '$amount' }
+        }
+      }
+    ]);
+
+    const resultpending = await sponsorshipRequestModel.aggregate([
+      {
+        $match: { request_state: { $nin: ['completed', 'denied'] } }
+      },
+      {
+        $group: {
+          _id: null, // Grouping by null groups all documents
+          totalAmount: { $sum: '$amount' }
+        }
+      }
+    ]);
+
+    const total_beneficiaries_assisted = await sponsorshipRequestModel.countDocuments({
+      request_state: 'completed'
+    });
+
+    const totalAmountCompleted = resultCompleted[0] ? resultCompleted[0].totalAmount : 0;
+    const totalAmountPending = resultpending[0] ? resultpending[0].totalAmount : 0;
+
+    const result = {
+      total_donations: totalAmountCompleted,
+      total_spent: totalAmountCompleted,
+      total_balance: totalAmountPending,
+      total_beneficiaries_assisted
+    };
+
+    return result;
+  } catch (error) {
+    console.error('Error getting total amount:', error);
+
+    return {
+      total_donations: 0,
+      total_spent: 0,
+      total_balance: 0,
+      total_beneficiaries_assisted: 0
+    };
+  }
+};
+
+export const fetchBeneficiries = async ({ user, param }) => {
+  let { page_no, no_of_requests, search, status } = param;
+
+  page_no = Number(page_no) || 1;
+  no_of_requests = Number(no_of_requests) || 20;
+
+  const filterData = {};
+
+  const query = typeof search !== 'undefined' ? search.trim() : false;
+
+  const rgx = (pattern) => new RegExp(`.*${pattern}.*`, 'i');
+  const searchRgx = rgx(query);
+
+  if (query) {
+    filterData['$or'] = [
+      { 'member_info.personal.member_name': searchRgx },
+      { 'member_info.contact.email': searchRgx },
+      { 'member_info.contact.phone': searchRgx },
+      { 'member_info.member_unique_id': searchRgx }
+    ];
+  }
+
+  if (status) {
+    filterData.request_state = status;
+  }
+
+  let fetchedData = await sponsorshipRequestModel.aggregate([
+    {
+      $lookup: {
+        from: 'organization_members',
+        as: 'member_info',
+        let: { memberId: '$beneficiary_id' },
+        pipeline: [
+          {
+            $match: {
+              $expr: { $and: [{ $eq: ['$_id', '$$memberId'] }] }
+            }
+          }
+        ]
+      }
+    },
+
+    { $unwind: '$member_info' },
+
+    { $match: { ...filterData } },
+
+    {
+      $facet: {
+        edges: [
+          { $sort: { createdAt: -1 } },
+          { $skip: (page_no - 1) * no_of_requests },
+          { $limit: no_of_requests }
+        ],
+        pageInfo: [{ $group: { _id: null, count: { $sum: 1 } } }]
+      }
+    }
+  ]);
+
+  const count = fetchedData[0].pageInfo.length ? fetchedData[0].pageInfo[0].count : 0;
+
+  let available_pages = Math.ceil(count / no_of_requests);
+
+  fetchedData = fetchedData[0].edges;
+
+  available_pages = available_pages ? available_pages : 0;
+
+  return {
+    page_no,
+    available_pages,
+    count,
+    fetched_data: fetchedData
+  };
 };
